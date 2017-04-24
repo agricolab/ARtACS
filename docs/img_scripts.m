5db2d4e3cd6ee1a0a72046963c491b4e868558a3
Robert Bauer <robertsadresse@gmx.de>
2017-04-24 18:33:43 +0200

    Write exemplary kernels section
    Causal, Symmetric and Tuning (\tau, N)

--- docs/img_scripts.m
+++ docs/img_scripts.m
@@ -17,18 +17,18 @@ tmp = reshape(tmp(247:end-254),500,[]);
 tmp = tmp-mean(mean(tmp));
 
 xaxis = rad2deg(2*pi*[0.002:0.002:1]);
-pick  = 1:250;
+pick  = 1:300;
 hold on
 h1 = plot(xaxis(pick),tmp(pick,:),'color',[.5 .5 .5],'linewidth',1)
 h2 = plot(xaxis(pick),mean(tmp(pick,:),2),'color','r','linewidth',2)
 set(gca,'XTICK',0:15:360,'XLIM',[60 120])
-set(gca,'YTICK',-5000:100:5000,'YLIM',[2750 3250],'YTICKLABEL',[])
+set(gca,'YTICK',-5000:100:5000,'YLIM',[2750 3300],'YTICKLABEL',[])
 xlabel('Phase in degree (°)')
 ylabel('Amplitude in a.u.')
 l1 = legend([h1(1),h2],'Periods','Average','location','northoutside')
-set(l1,'position',[0.15 .85 0 .125])
+set(l1,'position',[0.15 .85 0 .130])
 %title('Distortion')
-print(gcf,[printfolder,'distortion.png'],'-dpng')
+print(gcf,[printfolder,'intro/distortion.png'],'-dpng')
 
 figure
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
@@ -41,111 +41,179 @@ hold on
 h1 = plot(xaxis(pick),tmp(pick,:),'color',[.5 .5 .5],'linewidth',1)
 h2 = plot(xaxis(pick),mean(tmp(pick,:),2),'color','r','linewidth',2)
 set(gca,'XTICK',0:15:360,'XLIM',[60 120])
-set(gca,'YTICK',-5000:100:5000,'YLIM',[2750 3250],'YTICKLABEL',[])
+set(gca,'YTICK',-5000:100:5000,'YLIM',[2750 3300],'YTICKLABEL',[])
 xlabel('Phase in degree (°)')
 l1 = legend([h1(1),h2],'Periods','Average','location','northoutside')
-set(l1,'position',[0.85 .85 0 .125])
+set(l1,'position',[0.85 .85 0 .130])
 %title('Saturation')
-print(gcf,[printfolder,'saturation.png'],'-dpng')
+print(gcf,[printfolder,'intro/saturation.png'],'-dpng')
 
-%%
+%% Frequency Response of Examples
 close all
 
 NumberPeriods   = 10;
 tacsFreq        = 10;
 Fs              = 1000;
 
-filter.kernel.response(filter.kernel.create((NumberPeriods./2),tacsFreq,Fs,'sma'),Fs,1)
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'ave'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_sma.png'],'-dpng')
+print(gcf,[printfolder,'causal/kernel_ave.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'ave'),Fs,1)
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'linear'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_ave.png'],'-dpng')
+print(gcf,[printfolder,'causal/kernel_linear.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'linear'),Fs,1)
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'exp'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_linear.png'],'-dpng')
+print(gcf,[printfolder,'causal/kernel_exp.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'exp'),Fs,1)
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_exp.png'],'-dpng')
+print(gcf,[printfolder,'causal/kernel_gauss.png'],'-dpng')
+% --------------------
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,1)
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'ave'),Fs,2,30)
+set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+print(gcf,[printfolder,'causal/mag_ave.png'],'-dpng')
+
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'linear'),Fs,2,30)
+set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+print(gcf,[printfolder,'causal/mag_linear.png'],'-dpng')
+
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'exp'),Fs,2,30)
+set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+print(gcf,[printfolder,'causal/mag_exp.png'],'-dpng')
+
+filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,2,30)
+set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+print(gcf,[printfolder,'causal/mag_gauss.png'],'-dpng')
+
+%%
+
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'ave'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_gauss.png'],'-dpng')
+print(gcf,[printfolder,'sym/kernel_ave.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'cauchy'),Fs,1)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'linear'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
 set(gca,'ylim',[-.75 1.1])
-print(gcf,[printfolder,'kernel_cauchy.png'],'-dpng')
+print(gcf,[printfolder,'sym/kernel_linear.png'],'-dpng')
 
-
-%%%%
-
-filter.kernel.response(filter.kernel.create((NumberPeriods./2),tacsFreq,Fs,'sma'),Fs,2,25)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'exp'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_sma.png'],'-dpng')
+set(gca,'ylim',[-.75 1.1])
+print(gcf,[printfolder,'sym/kernel_exp.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'ave'),Fs,2,25)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,1)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_ave.png'],'-dpng')
+set(gca,'ylim',[-.75 1.1])
+print(gcf,[printfolder,'sym/kernel_gauss.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'linear'),Fs,2,25)
-set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_linear.png'],'-dpng')
+% --------------------
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'exp'),Fs,2,25)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'ave'),Fs,2,30)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_exp.png'],'-dpng')
+print(gcf,[printfolder,'sym/mag_ave.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,2,25)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'linear'),Fs,2,30)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_gauss.png'],'-dpng')
+print(gcf,[printfolder,'sym/mag_linear.png'],'-dpng')
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'cauchy'),Fs,2,25)
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'exp'),Fs,2,30)
 set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-print(gcf,[printfolder,'mag_cauchy.png'],'-dpng')
+print(gcf,[printfolder,'sym/mag_exp.png'],'-dpng')
+
+filter.kernel.response(filter.kernel.symmetric(NumberPeriods,tacsFreq,Fs,'gauss'),Fs,2,30)
+set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+print(gcf,[printfolder,'sym/mag_gauss.png'],'-dpng')
+
+%% Kernel Tuning
+tau     = [0,5,100,1000]
+
+close all
+
+for tau_idx = 1 : length(tau)
+    figure
+    hold on
+    set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+    filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'exp',tau(tau_idx)),Fs,1,[],gcf)    
+    set(gca,'ylim',[-1.1 1.1])
+    %set(get(gca,'children'),'Markerfacecolor',cmap{tau_idx});    
+    print(gcf,[printfolder,'tau/kernel_exp_',num2str(tau(tau_idx)),'.png'],'-dpng')
+    
+    figure
+    hold on
+    set(gcf,'Position',[100 500 400 300],'paperpositionmode','auto')
+    filter.kernel.response(filter.kernel.causal(NumberPeriods,tacsFreq,Fs,'exp',tau(tau_idx)),Fs,2,30,gcf)    
+   % set(get(gca,'children'),'Color',cmap{tau_idx});    
+    print(gcf,[printfolder,'tau/mag_exp_',num2str(tau(tau_idx)),'.png'],'-dpng')
+end
+
+
 %%
 close all
-for tau = 1:10
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'gauss',tau),Fs,1)
-set(gcf,'Position',[100 600 400 300],'paperpositionmode','auto')
-set(gca,'ylim',[-.75 1.1])
-
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'gauss',tau),Fs,2,25)
-set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-
+NumberPeriods = [1,10,50];
+for N = 1 : length(NumberPeriods)
+    figure
+    hold on
+    set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
+    filter.kernel.response(filter.kernel.causal(NumberPeriods(N),tacsFreq,Fs,'exp'),Fs,1,[],gcf)    
+    set(gca,'ylim',[-1.1 1.1])
+    %set(get(gca,'children'),'Markerfacecolor',cmap{tau_idx});    
+    print(gcf,[printfolder,'np/kernel_exp_',num2str(NumberPeriods(N)),'.png'],'-dpng')
+    
+    figure
+    hold on
+    set(gcf,'Position',[100 500 400 300],'paperpositionmode','auto')
+    filter.kernel.response(filter.kernel.causal(NumberPeriods(N),tacsFreq,Fs,'exp'),Fs,2,30,gcf)    
+   % set(get(gca,'children'),'Color',cmap{tau_idx});    
+    print(gcf,[printfolder,'np/mag_exp_',num2str(NumberPeriods(N)),'.png'],'-dpng')
+    
+        
 end
-%%
-close all
-for tau = 1: 10,
-% filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'exp',tau),Fs,1)
-% set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-% set(gca,'ylim',[-.75 1.1])
 
-filter.kernel.response(filter.kernel.create(NumberPeriods,tacsFreq,Fs,'exp',tau),Fs,2,25)
-set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-end
-%%
+   
 
-filter.kernel.response(filter.kernel.create(1,tacsFreq,Fs,'ave'),Fs,2,25)
-
-filter.kernel.response(filter.kernel.create(1,tacsFreq,Fs,'ave'),Fs,1)
 
 %%
-k = filter.kernel.create(NumberPeriods./2,10,Fs,'ave');
-k = conv(k,fliplr(k),'same');
-%k = (k+fliplr(k))./2;
-filter.kernel.response(k,Fs,2,25)
-set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-%print(gcf,[printfolder,'mag_exp.png'],'-dpng')
 
-filter.kernel.response(k,Fs,1)
-set(gcf,'Position',[100 100 400 300],'paperpositionmode','auto')
-%print(gcf,[printfolder,'kernel_exp.png'],'-dpng')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
